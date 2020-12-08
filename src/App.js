531fab7406e9bad2d9946b156f87d6a05d6020f9
steven payne <djnosey@hotmail.co.uk>
2020-12-08 15:25:44 +0100

    remove unused files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Private from "./pages/Private";
 
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Private from './pages/Private';
-
-import AnonRoute from './components/AnonRoute';
-import PrivateRoute from './components/PrivateRoute';
-
-
+import AnonRoute from "./components/AnonRoute";
+import PrivateRoute from "./components/PrivateRoute";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Navbar />
-
         <Switch>
           <Route exact path="/" component={Home} />
-
 
           <AnonRoute exact path="/signup" component={Signup} />
           <AnonRoute exact path="/login" component={Login} />
