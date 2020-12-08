42c76e6e4cc17ba6cfa8f9f92d57535b28454c6f
steven payne <djnosey@hotmail.co.uk>
2020-12-08 15:14:45 +0100

    swap src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Private from './pages/Private';
+
+import AnonRoute from './components/AnonRoute';
+import PrivateRoute from './components/PrivateRoute';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+
+
+          <AnonRoute exact path="/signup" component={Signup} />
+          <AnonRoute exact path="/login" component={Login} />
+
+          <PrivateRoute exact path="/private" component={Private} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
